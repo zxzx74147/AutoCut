bd713e32f4cc7b1a5e1890e661154235ed93143f
zhengxin <422583341@qq.com>
2015-11-04 13:47:22 +0800

    fix android:name="com.xxx" bug

--- src/com/baidu/autocut/tieba/item/XmlItem.java
+++ src/com/baidu/autocut/tieba/item/XmlItem.java
@@ -21,192 +21,199 @@ import com.baidu.autocut.tieba.util.UtilHelper;
 
 public class XmlItem extends BaseItem {
 
-	public XmlItem(String file){
-		init(file);
-	}
-	@Override
-	public boolean init(String file) {
-		key = file;
-		return true;
-	}
+    public XmlItem(String file) {
+        init(file);
+    }
 
-	@Override
-	public String getKey() {
-		// TODO Auto-generated method stub
-		return key;
-	}
+    @Override
+    public boolean init(String file) {
+        key = file;
+        return true;
+    }
 
-	@Override
-	public LinkedList<JavaItem> getJavaItem() {
-		if (mImportJavaItem == null) {
-			scanfSelf();
-		}
-		return mImportJavaItem;
-	}
+    @Override
+    public String getKey() {
+        // TODO Auto-generated method stub
+        return key;
+    }
 
-	@Override
-	public LinkedList<XmlItem> getXmlItem() {
-		if (mImportXmlItem == null) {
-			scanfSelf();
-		}
-		return mImportXmlItem;
-	}
+    @Override
+    public LinkedList<JavaItem> getJavaItem() {
+        if (mImportJavaItem == null) {
+            scanfSelf();
+        }
+        return mImportJavaItem;
+    }
 
-	@Override
-	public LinkedList<String> getImageItem() {
-		if (mImportImageItem == null) {
-			scanfSelf();
-		}
-		return mImportImageItem;
-	}
+    @Override
+    public LinkedList<XmlItem> getXmlItem() {
+        if (mImportXmlItem == null) {
+            scanfSelf();
+        }
+        return mImportXmlItem;
+    }
 
-	@Override
-	public boolean scanfSelf() {
-		mImportJavaItem = new LinkedList<JavaItem>();
-		mImportXmlItem = new LinkedList<XmlItem>();
-		mImportImageItem = new LinkedList<String>();
+    @Override
+    public LinkedList<String> getImageItem() {
+        if (mImportImageItem == null) {
+            scanfSelf();
+        }
+        return mImportImageItem;
+    }
 
-		BufferedReader reader = null;
-		String path = UtilHelper.getFilePath(key);
-		if (path == null) {
-			return true;
-		}
-		try {
-			InputStream stream = new FileInputStream(new File(path));
+    @Override
+    public boolean scanfSelf() {
+        mImportJavaItem = new LinkedList<JavaItem>();
+        mImportXmlItem = new LinkedList<XmlItem>();
+        mImportImageItem = new LinkedList<String>();
 
-			DocumentBuilderFactory factory = DocumentBuilderFactory
-					.newInstance();
-			DocumentBuilder builder;
-			builder = factory.newDocumentBuilder();
+        BufferedReader reader = null;
+        String path = UtilHelper.getFilePath(key);
+        if (path == null) {
+            return true;
+        }
+        try {
+            InputStream stream = new FileInputStream(new File(path));
 
-			Document document = builder.parse(stream);
-			Element root = document.getDocumentElement();
-			dfs(root);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		} finally {
-			try {
-				if (reader != null) {
-					reader.close();
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
+            DocumentBuilderFactory factory = DocumentBuilderFactory
+                    .newInstance();
+            DocumentBuilder builder;
+            builder = factory.newDocumentBuilder();
 
-		return true;
-	}
+            Document document = builder.parse(stream);
+            Element root = document.getDocumentElement();
+            dfs(root);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
 
-	//遍历xml树
-	private void dfs(Element ele) {
-		String tag = ele.getTagName();
-		if (HashedPoolMananger.checkCodeFile(tag)) {
-			// System.out.println(tag);
-			JavaItem java = new JavaItem(tag);
-			mImportJavaItem.add(java);
-		}
+        return true;
+    }
 
-		String value = ele.getFirstChild() != null ? ele.getFirstChild()
-				.getNodeValue() : null;
-		if (value != null) {
-			// System.out.println(value);
-			Matcher drawableMatcher = UtilHelper.mXmlDrawablePattern
-					.matcher(value);
-			while (drawableMatcher.find()) {
-				String group = drawableMatcher.group();
-				String imageName = group.substring(group.lastIndexOf("/") + 1);
-				if (HashedPoolMananger.checkImageFile(imageName)) {
-					// HashedPoolMananger.putHashedImageKey(imageName);
-					mImportImageItem.add(imageName);
-				}
-				if (HashedPoolMananger.checkCodeFile(imageName)) {
-					XmlItem item = new XmlItem(imageName);
-					mImportXmlItem.add(item);
-					// System.out.println(imageName);
-				}
+    //遍历xml树
+    private void dfs(Element ele) {
+        String tag = ele.getTagName();
+        if (HashedPoolMananger.checkCodeFile(tag)) {
+            // System.out.println(tag);
+            JavaItem java = new JavaItem(tag);
+            mImportJavaItem.add(java);
+        }
 
-			}
+        String value = ele.getFirstChild() != null ? ele.getFirstChild()
+                .getNodeValue() : null;
+        if (value != null) {
+            // System.out.println(value);
+            Matcher drawableMatcher = UtilHelper.mXmlDrawablePattern
+                    .matcher(value);
+            while (drawableMatcher.find()) {
+                String group = drawableMatcher.group();
+                String imageName = group.substring(group.lastIndexOf("/") + 1);
+                if (HashedPoolMananger.checkImageFile(imageName)) {
+                    // HashedPoolMananger.putHashedImageKey(imageName);
+                    mImportImageItem.add(imageName);
+                }
+                if (HashedPoolMananger.checkCodeFile(imageName)) {
+                    XmlItem item = new XmlItem(imageName);
+                    mImportXmlItem.add(item);
+                    // System.out.println(imageName);
+                }
 
-			Matcher layoutMatcher = UtilHelper.mXmlResPattern.matcher(value);
-			while (layoutMatcher.find()) {
-				String group = layoutMatcher.group();
-				String imageName = group.substring(group.lastIndexOf("/") + 1);
-				if (HashedPoolMananger.checkCodeFile(imageName)) {
-					XmlItem item = new XmlItem(imageName);
-					mImportXmlItem.add(item);
-					// System.out.println(imageName);
-				}
+            }
 
-			}
-		}
-		NamedNodeMap map = ele.getAttributes();
-		for (int i = 0; i < map.getLength(); i++) {
-			Node node = map.item(i);
-			String content = node.getTextContent();
-			// System.out.println(node.getTextContent());
-			Matcher drawableMatcher = UtilHelper.mXmlDrawablePattern
-					.matcher(content);
-			while (drawableMatcher.find()) {
-				String group = drawableMatcher.group();
-				String imageName = group.substring(group.lastIndexOf("/") + 1);
-				if (HashedPoolMananger.checkImageFile(imageName)) {
-					// HashedPoolMananger.putHashedImageKey(imageName);
-					mImportImageItem.add(imageName);
-					// System.out.println(imageName);
-				}
-				if (HashedPoolMananger.checkCodeFile(imageName)) {
-					XmlItem item = new XmlItem(imageName);
-					mImportXmlItem.add(item);
-					// System.out.println(imageName);
-				}
+            Matcher layoutMatcher = UtilHelper.mXmlResPattern.matcher(value);
+            while (layoutMatcher.find()) {
+                String group = layoutMatcher.group();
+                String imageName = group.substring(group.lastIndexOf("/") + 1);
+                if (HashedPoolMananger.checkCodeFile(imageName)) {
+                    XmlItem item = new XmlItem(imageName);
+                    mImportXmlItem.add(item);
+                    // System.out.println(imageName);
+                }
 
-				if (!imageName.endsWith("_1")) {
-					imageName = imageName + "_1";
-					if (HashedPoolMananger.checkImageFile(imageName)) {
-						// HashedPoolMananger.putHashedImageKey(imageName);
-						mImportImageItem.add(imageName);
-						// System.out.println(imageName);
-					}
-					if (HashedPoolMananger.checkCodeFile(imageName)) {
-						XmlItem item = new XmlItem(imageName);
-						mImportXmlItem.add(item);
-						// System.out.println(imageName);
-					}
-				}
+            }
+        }
+        NamedNodeMap map = ele.getAttributes();
+        for (int i = 0; i < map.getLength(); i++) {
+            Node node = map.item(i);
+            String content = node.getTextContent();
 
-			}
+            if (HashedPoolMananger.checkCodeFile(content)) {
+//				 System.out.println(content);
+                JavaItem java = new JavaItem(content);
+                mImportJavaItem.add(java);
+            }
+//			 System.out.println(node.getTextContent());
+            Matcher drawableMatcher = UtilHelper.mXmlDrawablePattern
+                    .matcher(content);
+            while (drawableMatcher.find()) {
+                String group = drawableMatcher.group();
+                String imageName = group.substring(group.lastIndexOf("/") + 1);
+                if (HashedPoolMananger.checkImageFile(imageName)) {
+                    // HashedPoolMananger.putHashedImageKey(imageName);
+                    mImportImageItem.add(imageName);
+                    // System.out.println(imageName);
+                }
+                if (HashedPoolMananger.checkCodeFile(imageName)) {
+                    XmlItem item = new XmlItem(imageName);
+                    mImportXmlItem.add(item);
+                    // System.out.println(imageName);
+                }
 
-			Matcher layoutMatcher = UtilHelper.mXmlResPattern
-					.matcher(content);
-			while (layoutMatcher.find()) {
-				String group = layoutMatcher.group();
-				String imageName = group.substring(group.lastIndexOf("/") + 1);
-				if (HashedPoolMananger.checkCodeFile(imageName)) {
-					XmlItem item = new XmlItem(imageName);
-					mImportXmlItem.add(item);
-					// System.out.println(imageName);
-				}
+                if (!imageName.endsWith("_1")) {
+                    imageName = imageName + "_1";
+                    if (HashedPoolMananger.checkImageFile(imageName)) {
+                        // HashedPoolMananger.putHashedImageKey(imageName);
+                        mImportImageItem.add(imageName);
+                        // System.out.println(imageName);
+                    }
+                    if (HashedPoolMananger.checkCodeFile(imageName)) {
+                        XmlItem item = new XmlItem(imageName);
+                        mImportXmlItem.add(item);
+                        // System.out.println(imageName);
+                    }
+                }
 
-			}
+            }
 
-			Matcher stringMatcher = UtilHelper.mXmlStringPattern
-					.matcher(content);
-			while (stringMatcher.find()) {
-				String group = stringMatcher.group();
-				String stringName = group.substring(group.lastIndexOf("/") + 1);
-				HashedPoolMananger.putHashedStringKey(stringName);
-				// System.out.println(stringName);
-			}
-		}
-		NodeList childs = ele.getChildNodes();
-		for (int i = 0; i < childs.getLength(); i++) {
-			Node item = childs.item(i);
-			if (item instanceof Element) {
-				dfs((Element) item);
-			}
-		}
-	}
+            Matcher layoutMatcher = UtilHelper.mXmlResPattern
+                    .matcher(content);
+            while (layoutMatcher.find()) {
+                String group = layoutMatcher.group();
+                String imageName = group.substring(group.lastIndexOf("/") + 1);
+                if (HashedPoolMananger.checkCodeFile(imageName)) {
+                    XmlItem item = new XmlItem(imageName);
+                    mImportXmlItem.add(item);
+                    // System.out.println(imageName);
+                }
+
+            }
+
+            Matcher stringMatcher = UtilHelper.mXmlStringPattern
+                    .matcher(content);
+            while (stringMatcher.find()) {
+                String group = stringMatcher.group();
+                String stringName = group.substring(group.lastIndexOf("/") + 1);
+                HashedPoolMananger.putHashedStringKey(stringName);
+                // System.out.println(stringName);
+            }
+        }
+        NodeList childs = ele.getChildNodes();
+        for (int i = 0; i < childs.getLength(); i++) {
+            Node item = childs.item(i);
+            if (item instanceof Element) {
+                dfs((Element) item);
+            }
+        }
+    }
 
 }
